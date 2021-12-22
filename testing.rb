16d96138fb218f445743b077402fd58d218dcab0
idg32 <95511222+idg32@users.noreply.github.com>
2021-12-22 17:14:02 -0500

    pooo

--- testing.rb
+++ testing.rb
@@ -1,5 +1,8 @@
-require 'cli_pix'
-file_path = "green_sq.png"
-image = CliPix::Image.from_file(file_path, autoscale: false)
-#image.size = [2,2]
-image.display
+# require 'cli_pix'
+# file_path = "green_sq.png"
+# image = CliPix::Image.from_file(file_path, autoscale: false)
+# #image.size = [2,2]
+# image.display
+
+require 'image_paradise'
+ImageParadise.confree_GUI
