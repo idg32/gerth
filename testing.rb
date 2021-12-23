99941b359487999d775a9e0ed3ea9bd8ccebea9d
idg32 <95511222+idg32@users.noreply.github.com>
2021-12-22 21:19:45 -0500

    ai GROWS

--- testing.rb
+++ testing.rb
@@ -20,7 +20,8 @@ def key_words()
         "WHEN",
         "WHERE",
         "WHY",
-        "HOW"
+        "HOW",
+        "WHO"
     ]
     included_pronoun_self = [
         "I",
@@ -34,10 +35,14 @@ def key_words()
         "YOURS",
         "YOU'RE"
     ]
-    return dictionary = [included_intro_words, included_prepositions, included_pronoun_self, included_pronoun_identitifier]
+    included_verbs_self = [
+        "AM",
+        "WAS"
+    ]
+    return dictionary = [included_intro_words, included_prepositions, included_pronoun_self, included_pronoun_identitifier,included_verbs_self]
 end
 
-#All know replies, etc...
+#All known replies, etc...
 def return_phrases()
     to_intros = Hash.new
     to_preposiions = Hash.new
@@ -53,8 +58,9 @@ def return_phrases()
     to_preposiions["WHERE"] = "For the St. Louis team "
     to_preposiions["WHY"] = "Wendnesday is why "
     to_preposiions["HOW"] = "Empty your pockets is how "
+    to_preposiions["WHO"] = "It isn't that imporant! "
 
-    to_pronoun_self["I"] = "You must'nt "
+    to_pronoun_self["I"] = "You "
     to_pronoun_self["MY"] = "Well I see, you are "
     to_pronoun_self["ME"] = "Nonsense with your "
     to_pronoun_self["I'M"] = "May I know why "
@@ -66,15 +72,15 @@ def find_key_word(inp)
     dict = key_words()
     counter = 0
     retener = ""
-    for j in 0..3
+    for j in 0..4
         counter = 0
-        while counter < dict[j].length
+        until counter == dict[j].length
             if inp.include?(dict[j][counter])
                 outsider = inp.each do |i| 
                     #puts i + " I"
                     if i.include?(dict[j][counter])
                         #puts i, j
-                        retener += i + " "
+                        retener += i + " " if !retener.include?(i)
                         #retener += return_phrases(i,j) + " "
                     end
                 end
@@ -83,16 +89,31 @@ def find_key_word(inp)
             counter += 1
         end
     end
-    retener = produce_syntax(retener,dict)
+    retener = produce_syntax(retener,dict,inp) unless retener == ""
     return retener
 end
 
-def produce_syntax(key_words,diction)
-    reply = return_phrases()
-    for i in 0..diction[0].length - 1
-        key_words.gsub(diction[0][i], reply[0]["HI"])
+def logic_here(words,term)
+    ret = ""
+    for i in 0..words.length - 1
+        ret = words[i+1] if words[i].include?(term)
     end
+    return ret
+end
+
+def produce_syntax(key_words,diction,origin)
+    reply = return_phrases()
+    #    [i]self, them[you]
+    flag = [false,false]
     puts key_words
+    maker = key_words.split(' ')
+    for i in 0..2
+        for k in 0..maker.length - 1
+            varent = maker[k]
+            #puts varent
+            key_words.gsub!(origin[k],reply[i][varent]) unless reply[i][varent] == nil
+        end
+    end
     return key_words
 end
 
@@ -102,7 +123,7 @@ def make_loop_start()
 end
 
 def make_loop_mid(inputs)
-    puts find_key_word(inputs)
+    puts find_key_word(inputs) unless find_key_word(inputs) == ""
     goto_end_er() unless inputs.include?("QUIT")
 end
 
